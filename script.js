88a66a31652d330cedb610b5a90f3e3a73443eaa
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-04-05 16:13:44 +0200

    Inserimento finestre per inserimento dati

---
+++ script.js
@@ -0,0 +1,15 @@
+//Inserimento finestre per inserire i dati
+const Nome = prompt ("Inserisci il nome");
+
+const Cognome = prompt ("Inserisci il cognome");
+
+const Colore = prompt ("Qual'è il tuo colore preferito?");
+
+//salvataggio delle informazioni una volta inserite
+console.log(Nome); 
+console.log(Cognome);
+console.log(Colore);
+
+//stampa il risultato
+
+const Password = Nome + Cognome + Colore + 22 ;
